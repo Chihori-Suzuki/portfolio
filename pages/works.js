feb84d8d70a1936b013c3cd16025e848c34f4f9e
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-06-01 23:51:11 -0700

    commit 0601

--- pages/works.js
+++ pages/works.js
@@ -3,6 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const useStyle = makeStyles({
   tool: {
@@ -10,18 +11,19 @@ const useStyle = makeStyles({
   },
 });
 
-// export async function getServerSideProps(context) {
-//   const { id } = context.query;
-//   const res = await fetch("https://portfolio.chiori.tech/works/" + id);
-//   const work = await res.json();
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  // const res = await fetch("https://portfolio.chiori.tech/works/" + id);
+  // const work = await res.json();
 
-//   return { props: { work } };
-// }
+  return { props: { id } };
+}
 
-export default function Works({}) {
+export default function Works({ id }) {
   const classes = useStyle();
+  // const params = useParams();
 
-  const work = [
+  const works = [
     {
       title: "Calorie and recipe",
       details:
@@ -62,29 +64,33 @@ export default function Works({}) {
       link: "https://github.com/Chihori-Suzuki",
     },
   ];
+  // const result = works.find((v) => v.id === id);
+  const target = works.find((key) => {
+    return key.id === id;
+  });
+
   return (
     <Grid container>
       <Grid item>
-        <Typography variant="h4" className={classes.tool} key={work.id}>
-          {work.title}
+        <Typography variant="h4" className={classes.tool}>
+          {target.title}
         </Typography>
         <Image
-          src={"/" + work.image}
+          src={"/" + target.image}
           width={600}
           height={400}
           className={classes.tool}
-          key={work.id}
         ></Image>
 
-        <Typography variant="body1" className={classes.tool} key={work.id}>
-          {work.details}
+        <Typography variant="body1" className={classes.tool}>
+          {target.details}
         </Typography>
 
         <Typography variant="h5" gutterBottom>
           Language
         </Typography>
-        <Typography variant="h6" className={classes.tool} key={work.id}>
-          {work.language}
+        <Typography variant="h6" className={classes.tool}>
+          {target.language}
         </Typography>
         <Typography variant="h5" gutterBottom>
           Link
@@ -92,11 +98,10 @@ export default function Works({}) {
         <Link
           variant="h6"
           className={classes.tool}
-          key={work.id}
-          href={work.link}
+          href={target.link}
           target="_blank"
         >
-          {work.link}
+          {target.link}
         </Link>
       </Grid>
     </Grid>
