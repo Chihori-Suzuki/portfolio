42e034717d47c2a95f51d124fcc3f568447e3637
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-07-13 18:19:34 -0700

    commit0713 data fixed

--- data/data.js
+++ data/data.js
@@ -10,32 +10,13 @@ export const works = [
     link: "https://github.com/Chihori-Suzuki/Recipe-and-Calorie-Manager-App",
   },
   {
-    title: "something project 2",
+    title: "music group portfolio",
     details:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.",
-    category: "sss",
+      "portfolio of Chorus group. It has CRUD system and login / sign up system. If you are the one of the member of the group, you can update the blog page. ",
+    category: "ReactJs",
     id: 1,
     image: "/view1.jpg",
-    language: "Javascript",
-    link: "https://github.com/Chihori-Suzuki",
-  },
-  {
-    title: "something project 3",
-    details:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    category: "sss",
-    id: 2,
-    image: "/flower2.jpg",
-    language: "Swift",
-    link: "https://github.com/Chihori-Suzuki",
-  },
-  {
-    title: "Portfolio",
-    details: "making portfolio",
-    category: "sss",
-    id: 3,
-    image: "/view2.jpg",
-    language: "Swift",
-    link: "https://github.com/Chihori-Suzuki",
+    language: "Javascript/HTML/CSS",
+    link: "https://github.com/Chihori-Suzuki/LaFontaine-portfolio",
   },
 ];
