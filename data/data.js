92c95e8ebbd8b0a2bd6a1976bcfcac31d3378824
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2022-02-08 16:56:31 -0800

    refactoring

--- data/data.js
+++ data/data.js
@@ -1,31 +1,31 @@
 export const works = [
-  {
-    title: "Calorie and Recipe Manager App",
-    details:
-      "You can create new meal recipes and calculate how much calorie it takes. You can knowhow much and how often to eat the ingredient (summary). You can generate your own daily meal plan to avoid eating unbalanced nutrition meals. It's good for anyone who want to eat healthy or do diet. You can think about your healthy meal anytime using smartphone.",
-    category: "iOS",
-    id: 0,
-    image: "/recipe-image.png",
-    language: "Swift, GitHub",
-    link: "https://github.com/Chihori-Suzuki/Recipe-and-Calorie-Manager-App",
-  },
-  {
-    title: "music group portfolio",
-    details:
-      "portfolio of Chorus group. It has CRUD system and login / sign up system. If you are the one of the member of the group, you can update the blog page. ",
-    category: "ReactJs",
-    id: 1,
-    image: "/view1.jpg",
-    language: "Javascript/HTML/CSS",
-    link: "https://lafontaine-web-36a6f.web.app/",
-  },
-  {
-    title: "Tony's coffee web site",
-    details: "Web site of Coffee shop.",
-    category: "ReactJs",
-    id: 2,
-    image: "/tony.png",
-    language: "Typescript/HTML/CSS",
-    link: "https://tonyscoffee.ca/",
-  },
+    {
+        title: "Calorie and Recipe Manager App",
+        details:
+            "You can create new meal recipes and calculate how much calorie it takes. You can knowhow much and how often to eat the ingredient (summary). You can generate your own daily meal plan to avoid eating unbalanced nutrition meals. It's good for anyone who want to eat healthy or do diet. You can think about your healthy meal anytime using smartphone.",
+        category: "iOS",
+        id: 0,
+        image: "/recipe-image.png",
+        stacks: "Swift, GitHub",
+        link: "https://github.com/Chihori-Suzuki/Recipe-and-Calorie-Manager-App",
+    },
+    {
+        title: "music group portfolio",
+        details:
+            "portfolio of Chorus group. It has CRUD system and login / sign up system. If you are the one of the member of the group, you can update the blog page. ",
+        category: "ReactJs",
+        id: 1,
+        image: "/view1.jpg",
+        stacks: "React, Material-ui, firebase",
+        link: "https://lafontaine-web-36a6f.web.app/",
+    },
+    {
+        title: "Tony's coffee web site",
+        details: "Web site of Coffee shop.",
+        category: "ReactJs",
+        id: 2,
+        image: "/tony.png",
+        stacks: "Typescript, React, SCSS",
+        link: "https://tonyscoffee.ca/",
+    },
 ];
--- pages/works.js
+++ pages/works.js
@@ -6,159 +6,164 @@ import { works } from "../data/data";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 const useStyle = makeStyles((theme) => {
-  return {
-    grid: {
-      paddingTop: "5%",
-      paddingBottom: "10%",
-      [theme.breakpoints.down("sm")]: {
-        paddingLeft: "10%",
-        paddingRight: "10%",
-      },
-      [theme.breakpoints.up("sm")]: {
-        paddingLeft: "25%",
-        paddingRight: "25%",
-      },
-    },
-    paper: {
-      display: "flex",
-      background: "rgba(255, 255, 255, 0.7)",
-      borderRadius: 0,
-    },
+    return {
+        grid: {
+            paddingTop: "5%",
+            paddingBottom: "10%",
+            [theme.breakpoints.down("sm")]: {
+                paddingLeft: "10%",
+                paddingRight: "10%",
+            },
+            [theme.breakpoints.up("sm")]: {
+                paddingLeft: "25%",
+                paddingRight: "25%",
+            },
+        },
+        paper: {
+            display: "flex",
+            background: "rgba(255, 255, 255, 0.7)",
+            borderRadius: 0,
+        },
 
-    appBar: {
-      marginTop: "5%",
-      paddingLeft: "5%",
-    },
-    textColor: {
-      marginLeft: "3%",
-      padding: "1%",
-      color: "black",
-      fontSize: "20px",
-      "&:hover": {
-        background: "linear-gradient(to right bottom, #9BA8FC, #F2BEE4)",
-      },
-    },
-    typography2: {
-      textAlign: "center",
-      [theme.breakpoints.down("sm")]: {
-        fontSize: "30px",
-        fontWeight: "300",
-        color: "#590477",
-      },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "40px",
-        fontWeight: "400",
-        color: "#590477",
-      },
-      padding: "5%",
-    },
-    subtitle: {
-      marginTop: "7%",
-      marginBottom: "3%",
-      [theme.breakpoints.down("sm")]: {
-        fontSize: "20px",
-        fontWeight: "300",
-      },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "30px",
-        fontWeight: "350",
-      },
-    },
-    body: {
-      [theme.breakpoints.down("sm")]: {
-        fontSize: "15px",
-        fontWeight: "300",
-      },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "18px",
-        fontWeight: "350",
-      },
-    },
-    body2: {
-      [theme.breakpoints.down("sm")]: {
-        fontSize: "17px",
-        fontWeight: "300",
-      },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "25px",
-        fontWeight: "350",
-      },
-    },
-    body3: {
-      [theme.breakpoints.down("sm")]: {
-        fontSize: "15px",
-        fontWeight: "300",
-      },
-      [theme.breakpoints.up("sm")]: {
-        fontSize: "20px",
-        fontWeight: "350",
-      },
-    },
-  };
+        appBar: {
+            marginTop: "5%",
+            paddingLeft: "5%",
+        },
+        textColor: {
+            marginLeft: "3%",
+            padding: "1%",
+            color: "black",
+            fontSize: "20px",
+            "&:hover": {
+                background:
+                    "linear-gradient(to right bottom, #9BA8FC, #F2BEE4)",
+            },
+        },
+        typography2: {
+            textAlign: "center",
+            [theme.breakpoints.down("sm")]: {
+                fontSize: "30px",
+                fontWeight: "300",
+                color: "#590477",
+            },
+            [theme.breakpoints.up("sm")]: {
+                fontSize: "40px",
+                fontWeight: "400",
+                color: "#590477",
+            },
+            padding: "5%",
+        },
+        subtitle: {
+            marginTop: "7%",
+            marginBottom: "3%",
+            [theme.breakpoints.down("sm")]: {
+                fontSize: "20px",
+                fontWeight: "300",
+            },
+            [theme.breakpoints.up("sm")]: {
+                fontSize: "30px",
+                fontWeight: "350",
+            },
+        },
+        body: {
+            [theme.breakpoints.down("sm")]: {
+                fontSize: "15px",
+                fontWeight: "300",
+            },
+            [theme.breakpoints.up("sm")]: {
+                fontSize: "18px",
+                fontWeight: "350",
+            },
+        },
+        body2: {
+            [theme.breakpoints.down("sm")]: {
+                fontSize: "17px",
+                fontWeight: "300",
+            },
+            [theme.breakpoints.up("sm")]: {
+                fontSize: "25px",
+                fontWeight: "350",
+            },
+        },
+        body3: {
+            [theme.breakpoints.down("sm")]: {
+                fontSize: "15px",
+                fontWeight: "300",
+            },
+            [theme.breakpoints.up("sm")]: {
+                fontSize: "20px",
+                fontWeight: "350",
+            },
+        },
+    };
 });
 
 export async function getServerSideProps(context) {
-  const { id } = context.query;
-  return { props: { id } };
+    const { id } = context.query;
+    return { props: { id } };
 }
 
 export default function Works({ id }) {
-  const classes = useStyle();
+    const classes = useStyle();
 
-  return (
-    <Paper className={classes.paper}>
-      <AppBar color="transparent" className={classes.appBar} elevation={0}>
-        <Toolbar>
-          <ArrowBackIcon />
-          <Link className={classes.textColor} href="/">
-            BACK
-          </Link>
-        </Toolbar>
-      </AppBar>
+    return (
+        <Paper className={classes.paper}>
+            <AppBar
+                color="transparent"
+                className={classes.appBar}
+                elevation={0}
+            >
+                <Toolbar>
+                    <ArrowBackIcon />
+                    <Link className={classes.textColor} href="/">
+                        BACK
+                    </Link>
+                </Toolbar>
+            </AppBar>
 
-      <Grid
-        container
-        className={classes.grid}
-        direction="column"
-        alignItems="center"
-        justify="space-between"
-      >
-        <h2 className={classes.typography2}>{works[id].title}</h2>
-        <Image
-          src={works[id].image}
-          width={600}
-          height={400}
-          className={classes.margin}
-        ></Image>
-        <Box mt="5%" mx="7%">
-          <Typography
-            variant="body1"
-            gutterBottom
-            className={classes.body}
-            word
-          >
-            {works[id].details}
-          </Typography>
-        </Box>
-        <Typography variant="h5" className={classes.subtitle}>
-          Language, Software
-        </Typography>
-        <Typography variant="h6" gutterBottom className={classes.body2}>
-          {works[id].language}
-        </Typography>
-        <Typography variant="h5" className={classes.subtitle}>
-          Link
-        </Typography>
-        <Link
-          variant="h6"
-          href={works[id].link}
-          target="_blank"
-          className={classes.body3}
-          color="#000000"
-        >
-          {works[id].link}
-        </Link>
-      </Grid>
-    </Paper>
-  );
+            <Grid
+                container
+                className={classes.grid}
+                direction="column"
+                alignItems="center"
+                justify="space-between"
+            >
+                <h2 className={classes.typography2}>{works[id].title}</h2>
+                <Image
+                    src={works[id].image}
+                    width={600}
+                    height={400}
+                    className={classes.margin}
+                ></Image>
+                <Box mt="5%" mx="7%">
+                    <Typography
+                        variant="body1"
+                        gutterBottom
+                        className={classes.body}
+                        word
+                    >
+                        {works[id].details}
+                    </Typography>
+                </Box>
+                <Typography variant="h5" className={classes.subtitle}>
+                    Language, Software
+                </Typography>
+                <Typography variant="h6" gutterBottom className={classes.body2}>
+                    {works[id].stacks}
+                </Typography>
+                <Typography variant="h5" className={classes.subtitle}>
+                    Link
+                </Typography>
+                <Link
+                    variant="h6"
+                    href={works[id].link}
+                    target="_blank"
+                    className={classes.body3}
+                    color="#000000"
+                >
+                    {works[id].link}
+                </Link>
+            </Grid>
+        </Paper>
+    );
 }
